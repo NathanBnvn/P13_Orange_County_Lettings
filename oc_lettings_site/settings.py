28e04e97f70f7c956b7d2655516be1abf978ee03
Nathan Bienvenu <bienvenu.nathan72@gmail.com>
2022-08-31 10:53:58 +0200

    new cmd to deploy in config

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,5 +1,7 @@
 import os
+# import sentry_sdk
 from decouple import config
+# from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -110,3 +112,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Sentry
+
+# sentry_sdk.init(
+#     dsn= config(SENTRY_DNS),
+#     integrations=[
+#         DjangoIntegration(),
+#     ],
+
+#     # Set traces_sample_rate to 1.0 to capture 100%
+#     # of transactions for performance monitoring.
+#     # We recommend adjusting this value in production.
+#     traces_sample_rate=1.0,
+
+#     # If you wish to associate users to errors (assuming you are using
+#     # django.contrib.auth) you may enable sending PII data.
+#     send_default_pii=True
+# )
